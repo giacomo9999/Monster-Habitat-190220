f868efe86a637a40b25bdcb1b3ed7b3d7dd0ba46
James Gary <gmarchesi888@gmail.com>
2019-02-21 13:16:10 -0500

    Added monsterController.

--- controllers/MonsterController.js
+++ controllers/MonsterController.js
@@ -12,3 +12,79 @@ monsterController.list = (req, res) => {
     }
   });
 };
+
+monsterController.show = (req, res) => {
+  Monster.findOne({ _id: req.params.id }).exec((err, monster) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("../views/monsters/show", { monster: monster });
+    }
+  });
+};
+
+monsterController.create = (req, res) => {
+  res.render("../views/monsters/create");
+};
+
+monsterController.save = (req, res) => {
+  const monster = new Monster(req.body);
+  monster.save(err => {
+    if (err) {
+      console.log(err);
+      res.render("../views/monsters/create");
+    } else {
+      console.log("Successfully created a monster.");
+      res.redirect("/monsters/show" + monster._id);
+    }
+  });
+};
+
+monsterController.edit = (req, res) => {
+  Monster.findOne({ _id: req.params.id }).exec((err, monster) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("../views/monsters/edit", { monster: monster });
+    }
+  });
+};
+
+monsterController.update = (req, res) => {
+  Monster.findByIdAndUpdate(
+    req.params.id,
+    {
+      $set: {
+        api_index: req.body.api_index,
+        name: req.body.name,
+        number_appearing: req.body.name,
+        alignment: req.body.name,
+        hit_dice: req.body.name,
+        type: req.body.name,
+        frequency: req.body.name,
+        habitat: req.body.name
+      }
+    },
+    { new: true },
+    (err, monster) => {
+      if (err) {
+        console.log(err);
+        res.render("../views/monsters/edit", { monster: req.body });
+      }
+      res.redirect("/monsters/show/" + monster._id);
+    }
+  );
+};
+
+monsterController.delete = (req, res) => {
+  Monster.remove({ _id: req.params.id }, err => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Monster Deleted!");
+      res.redirect("/monsters");
+    }
+  });
+};
+
+module.exports = monsterController;
