6b1bbdcd8f4351e2120dba5a81bc42afe4a425e6
James Gary <gmarchesi888@gmail.com>
2019-02-21 15:30:38 -0500

    app.use express.static serves static assets from named directory.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Jimbö Triümphant' });
+  res.render('index', { title: 'Home Index' });
 });
 
 module.exports = router;
